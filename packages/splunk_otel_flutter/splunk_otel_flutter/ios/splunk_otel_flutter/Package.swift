c5ad1f3fd91c31b83147aeab5b552eccd231dff0
Zdeněk Vaníček <zdenda@cisco.com>
2025-10-07 20:36:26 +0200

    feat(iosSdk): add ios agent sdk

--- packages/splunk_otel_flutter/splunk_otel_flutter/ios/splunk_otel_flutter/Package.swift
+++ packages/splunk_otel_flutter/splunk_otel_flutter/ios/splunk_otel_flutter/Package.swift
@@ -6,27 +6,23 @@ import PackageDescription
 let package = Package(
     name: "splunk_otel_flutter",
     platforms: [
-        .iOS("13.0")
+        .iOS(.v15)
     ],
     products: [
-        .library(name: "splunk-otel-flutter", targets: ["splunk_otel_flutter"])
+        .library(name: "splunk-otel-flutter",
+        targets: ["splunk_otel_flutter"]
+        )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/signalfx/splunk-otel-ios", from: "2.0.0")
+    ],
     targets: [
         .target(
             name: "splunk_otel_flutter",
-            dependencies: [],
-            resources: [
-                // If your plugin requires a privacy manifest, for example if it uses any required
-                // reason APIs, update the PrivacyInfo.xcprivacy file to describe your plugin's
-                // privacy impact, and then uncomment these lines. For more information, see
-                // https://developer.apple.com/documentation/bundleresources/privacy_manifest_files
-                // .process("PrivacyInfo.xcprivacy"),
-
-                // If you have other resources that need to be bundled with your plugin, refer to
-                // the following instructions to add them:
-                // https://developer.apple.com/documentation/xcode/bundling-resources-with-a-swift-package
-            ]
+            dependencies: [
+                .product(name: "SplunkAgent", package: "splunk-otel-ios")
+            ],
+            resources: []
         )
     ]
 )
--- packages/splunk_otel_flutter/splunk_otel_flutter/ios/splunk_otel_flutter/Sources/splunk_otel_flutter/SplunkOtelFlutterPlugin.swift
+++ packages/splunk_otel_flutter/splunk_otel_flutter/ios/splunk_otel_flutter/Sources/splunk_otel_flutter/SplunkOtelFlutterPlugin.swift
@@ -1,10 +1,55 @@
 import Flutter
 import UIKit
+import SplunkAgent
+import OpenTelemetryApi
+import SplunkSlowFrameDetector
 
 public class SplunkOtelFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "splunk_otel_flutter", binaryMessenger: registrar.messenger())
+ let channel = FlutterMethodChannel(name: "splunkotelflutter", binaryMessenger: registrar.messenger())
     let instance = SplunkOtelFlutterPlugin()
+
+      let endpointConfig = EndpointConfiguration(
+          realm: "",
+          rumAccessToken: ""
+      )
+
+      let agentConfig = AgentConfiguration(
+          endpoint: endpointConfig,
+          appName: "Flutter agent app",
+          deploymentEnvironment: "test-ios"
+      )
+          .enableDebugLogging(true)
+          .globalAttributes(MutableAttributes(dictionary: [
+              "teststring": .string("value"),
+              "testint": .int(100)]))
+          .spanInterceptor { spanData in
+              var attributes = spanData.attributes
+              attributes["test_attribute"] = AttributeValue("test_value")
+
+              var modifiedSpan = spanData
+              modifiedSpan.settingAttributes(attributes)
+
+              return modifiedSpan
+          }
+      do {
+          _ = try SplunkRum.install(with: agentConfig,
+                                    moduleConfigurations: [SlowFrameDetectorConfiguration(isEnabled: true)])
+      } catch {
+          print("Unable to start the Splunk agent, error: \(error)")
+      }
+
+      // Navigation Instrumentation
+      SplunkRum.shared.navigation.preferences.enableAutomatedTracking = true
+      SplunkRum.shared.slowFrameDetector
+
+      // Start session replay
+      SplunkRum.shared.sessionReplay.start()
+
+      // API to update Global Attributes
+      SplunkRum.shared.globalAttributes.setBool(true, for: "isWorkingHard")
+      SplunkRum.shared.globalAttributes[string: "secret"] = "Monster"
+
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
